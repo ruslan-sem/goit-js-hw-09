5a2ff6835ea377e8f6cb90e9078a0fde96ad26d9
Ruslan Samoylenko <ruslan.sem@gmail.com>
2022-11-15 23:00:30 +0200

    Задание 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
+}
+const form = document.querySelector('.form');
+form.addEventListener('submit', onSubmit);
+function onSubmit(event) {
+  event.preventDefault();
+  let {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+  delay = Number(delay.value);
+  step = Number(step.value);
+  amount = Number(amount.value);
+  for (let i = 1; i <= amount; i += 1, delay += step) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
