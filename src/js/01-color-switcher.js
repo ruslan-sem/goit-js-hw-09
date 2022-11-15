847f2a69c3f929144e303a6ca709f7dba9250639
Ruslan Samoylenko <ruslan.sem@gmail.com>
2022-11-15 17:43:31 +0200

    Задание 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId;
 
+startBtn.addEventListener('click', onStartBtn);
+stopBtn.addEventListener('click', onStopBtn);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartBtn(event) {
+  startBtn.setAttribute('disabled', true);
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopBtn(event) {
+  startBtn.removeAttribute('disabled');
+  clearInterval(timerId);
+}
